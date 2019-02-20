55f1cb96c146a41f6a36da8f05c4db0835a37f90
McSmallpox <ryanwands18@gmail.com>
2019-02-20 16:18:09 -0700

    Initial commit

---
+++ src/edu/cnm/deepdive/life/controller/Life.java
@@ -0,0 +1,14 @@
+package edu.cnm.deepdive.life.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+
+public class Life {
+
+  @FXML
+  private ScrollPane viewScroller;
+
+  @FXML
+  private Button start;
+}
